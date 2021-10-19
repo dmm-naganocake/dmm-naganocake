f14fa4b9db8694d96e24c38d6ab3cdc9ae9856ba
tmatsu <tmatsunaga65@gmail.com>
2021-10-19 22:08:53 +0000

    合計金額の計算が完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -38,13 +38,21 @@ class Public::OrdersController < ApplicationController
   #     render :check
         @order = Order.new(order_params)
         @cart_items = CartItem.where(customer_id: current_customer.id)
+
+        # @cart_items = current_customer.cart_itemsとしても同じはず
+        @total_payment = 0
+        @cart_items.each do |cart_item|
+          @total_payment = @total_payment + cart_item.item.price * cart_item.amount
+        end
+        @total_payment = @total_payment + 800
+
         if params[:order][:address_number] == '0'
           @order.postal_code = current_customer.postal_code
           @order.address = current_customer.address
           @order.name = current_customer.last_name + current_customer.first_name
           @order.shipping_cost = 800
           @order.status = 0
-          @order.total_payment = 5000
+          @order.total_payment = @total_payment
           @order.payment_method = params[:order][:payment_method]
         elsif params[:order][:address_number] == '1'
           @selected_address = Address.find(params[:order][:address_id])
@@ -53,7 +61,7 @@ class Public::OrdersController < ApplicationController
           @order.name = @selected_address.name
           @order.shipping_cost = 800
           @order.status = 0
-          @order.total_payment = 5000
+          @order.total_payment = @total_payment
           @order.payment_method = params[:order][:payment_method]
         elsif params[:order][:address_number] == '2'
           @order.postal_code = params[:order][:postal_code]
@@ -61,7 +69,7 @@ class Public::OrdersController < ApplicationController
           @order.name = params[:order][:name]
           @order.shipping_cost = 800
           @order.status = 0
-          @order.total_payment = 5000
+          @order.total_payment = @total_payment
           @order.payment_method = params[:order][:payment_method]
         else
         end
