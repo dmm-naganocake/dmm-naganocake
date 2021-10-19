78aa4c59269d7f4dcdc43bd3ecb133d53b379b31
tmatsu <tmatsunaga65@gmail.com>
2021-10-19 06:30:50 +0000

    会員編集ページが完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,5 +14,14 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:email,:phone_number,:postal_code,:address)
   end
 end
