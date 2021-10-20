6103ef57b6fd6eb31c1bb756bf9431c41c4a5b82
sanamaruno <xwkpm269@gmail.com>
2021-10-20 07:37:20 +0000

    Fix routes

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -23,9 +23,16 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      redirect_to request.referer,notice:"変更を保存できませんでした"
+    end
   end
 
   private
