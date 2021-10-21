99cad63801644adc3f46ce6a80cc34d477145719
tmatsu <tmatsunaga65@gmail.com>
2021-10-21 03:39:34 +0000

    微修正

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -2,8 +2,8 @@ class Public::GenresController < ApplicationController
   def show
     @genres = Genre.all
     @genre =Genre.find(params[:id])
-    @item = Item.find(params[:id]) 
+    # @item = Item.find(params[:id])
   end
-  
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
-  has_many :order, dependent: :destroy
+  has_many :order_details, dependent: :destroy
+
 
   attachment :image
 
