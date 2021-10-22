eb5a67378a8e0d47711833e9c2b79170000c42a6
tmatsu <tmatsunaga65@gmail.com>
2021-10-22 08:43:08 +0000

    管理者側の注文履歴機能の更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,8 +11,14 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-
+    @order=Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to request.referer
   end
 
+  private
 
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
