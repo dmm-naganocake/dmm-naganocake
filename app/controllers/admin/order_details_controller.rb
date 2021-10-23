13bd75098facf0ecf08aeef71cf65eac897d8633
rikuzyoukurabu.gmail.com <ec2-user@ip-172-31-22-246.ec2.internal>
2021-10-23 02:21:55 +0000

    注文ステータスhennkou

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,11 +4,17 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
+    @order = Order.find(session[:order_id])
+    @order_details = @order.order_details
+    
     if @order_detail.making_status == "in_production"
+      # 制作中
       @order_detail.order.status = "in_production"
+      # 制作中
       @order_detail.order.save
     end
-    if @order_detail.making_status == "production_complete"
+    
+    if @order_details.where(making_status: "production_complete").count == @order_details.count
       @order_detail.order.status = "preparing_delivery"
       @order_detail.order.save
     end
